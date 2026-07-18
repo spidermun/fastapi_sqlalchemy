61e14b43e046b1966cf35b918c25bffdb2c133c6
Michał <mtrela2222@gmail.com>
2026-07-18 13:18:40 +0200

    add configuration files for FastAPI project setup

---
+++ src/config.py
@@ -0,0 +1,9 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
